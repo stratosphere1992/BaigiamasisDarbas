8f9ddf41bd4bd248c6cec23023d3172f9b8a5dd2
stratosphere1992 <tadas.kardonas@gmail.com>
2023-01-25 09:16:45 +0200

    v1
    Prisijungimo, registracijos ir slaptažodžio atstatymo sistema. El. pašto patvirtinimas neįtrauktas.

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.app')
+
+@section('content')
+<!-- Pastaba, nuo 9-os Laravel versijos php artisan route:list --compact neveikia, naudoti php artisan route:list -->
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="text-center mt-2"><h4>{{ __('Register') }}</h4></div>
+                <div class="card-body">
+                    <!-- Forma. Žemiau "Cross-Site Request Forgery" saugos mechanizmas -->
+                    <form method="POST" action="{{ route('register') }}">@csrf
+                        <!-- Vartotojo vardo įvedimas -->
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
+                                <!-- Laukelio pranešimas blogai pateikus duomenis. Svarbu, komentaruose nenaudti eta simbolio pvz.: eta_error, kodas neveikia!!! -->
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <!-- Vartotojo el. pašto įvedimas -->
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <!-- Vartotojo slaptažodio įvedimas -->
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <!-- Slaptažodžio patvirtinimas -->
+                        <div class="row mb-3">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+                        <!-- Patvirtinimo mygtukas -->
+                        <div class="row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
